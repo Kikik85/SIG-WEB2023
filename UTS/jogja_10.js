25ccd5996948f9d4198d114fc658cdd1cfa21473
Kikik85 <rizqi.adi2002@mail.ugm.ac.id>
2023-10-09 15:05:32 +0700

    UTS

---
+++ UTS/jogja_10.js
@@ -0,0 +1,17 @@
+var jogja_10 = {
+    "type": "FeatureCollection",
+    "name": "10",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "No": 1, "Nama": "Lapangan Pancasila", "Lintang": -7.76384, "Bujur": 110.37279, "X": 430841, "Y": 9141759 }, "geometry": { "type": "Point", "coordinates": [ 110.372786051293872, -7.763837432058026 ] } },
+    { "type": "Feature", "properties": { "No": 2, "Nama": "Geodesi", "Lintang": 7.77093, "Bujur": 110.37768, "X": 431382, "Y": 9140976 }, "geometry": { "type": "Point", "coordinates": [ 110.37768184355015, -7.77092696737593 ] } },
+    { "type": "Feature", "properties": { "No": 3, "Nama": "Kos", "Lintang": -7.75876, "Bujur": 110.3751, "X": 431095, "Y": 9142321 }, "geometry": { "type": "Point", "coordinates": [ 110.375097031003477, -7.758757468656711 ] } },
+    { "type": "Feature", "properties": { "No": 4, "Nama": "FKG", "Lintang": -7.77063, "Bujur": 110.37451, "X": 431032, "Y": 9141008 }, "geometry": { "type": "Point", "coordinates": [ 110.374508141093074, -7.77063286247803 ] } },
+    { "type": "Feature", "properties": { "No": 5, "Nama": "Tugu Teknik", "Lintang": -7.76598, "Bujur": 110.37383, "X": 430956, "Y": 9141522 }, "geometry": { "type": "Point", "coordinates": [ 110.373825788221751, -7.765982660558914 ] } },
+    { "type": "Feature", "properties": { "No": 6, "Nama": "Sardjito", "Lintang": -7.76798, "Bujur": 110.37383, "X": 430957, "Y": 9141301 }, "geometry": { "type": "Point", "coordinates": [ 110.373831896806578, -7.767981643399209 ] } },
+    { "type": "Feature", "properties": { "No": 7, "Nama": "Portal Teknik", "Lintang": -7.7662, "Bujur": 110.37418, "X": 430995, "Y": 9141498 }, "geometry": { "type": "Point", "coordinates": [ 110.374179151550592, -7.766200264062992 ] } },
+    { "type": "Feature", "properties": { "No": 8, "Nama": "Lempuyangan", "Lintang": -7.7902, "Bujur": 110.37551, "X": 431146, "Y": 9138845 }, "geometry": { "type": "Point", "coordinates": [ 110.375513054773492, -7.79019895191932 ] } },
+    { "type": "Feature", "properties": { "No": 9, "Nama": "YK", "Lintang": -7.78924, "Bujur": 110.36352, "X": 429823, "Y": 9138949 }, "geometry": { "type": "Point", "coordinates": [ 110.363515727978438, -7.789240413607016 ] } },
+    { "type": "Feature", "properties": { "No": 10, "Nama": "Masjid MPR", "Lintang": -7.75919, "Bujur": 110.37481, "X": 431063, "Y": 9142273 }, "geometry": { "type": "Point", "coordinates": [ 110.374806191885639, -7.759191207919694 ] } }
+    ]
+    }
